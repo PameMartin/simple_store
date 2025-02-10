e74c5850d0dfa58855f628407f6467b2b6deb7c1
Pamela Martin <pmartin3@academic.rrc.ca>
2025-02-10 11:10:55 -0600

    Generating the Category model with a string named name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_10_142251) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_10_170957) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
