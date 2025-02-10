7a8cc740d8fdee19e1e5d00636eb94305e5cf9ea
Pamela Martin <pmartin3@academic.rrc.ca>
2025-02-10 13:24:00 -0600

    Loading categories along side products in the products controller's index to avoid  N+1 issue

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.include(:category).all
   end
   def show
     @product = Product.find(params[:id])
