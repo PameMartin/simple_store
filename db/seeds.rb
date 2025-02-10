5883785cbb9c9d3a9c162e53bc35db23e35abc69
Pamela Martin <pmartin3@academic.rrc.ca>
2025-02-10 13:00:04 -0600

    Modified my seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,39 @@
 require "csv"
 Product.destroy_all
+Category.destroy_all
 
-676.times do
-  Product.create(
-    title: Faker::Commerce.unique.product_name,
+filename = Rails.root.join('db/products.csv')
+csv_data = File.read(filename)
 
-    price: Faker::Commerce.price,
+products = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
 
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
-  )
+# 676.times do
+#   Product.create(
+#     title: Faker::Commerce.unique.product_name,
+
+#     price: Faker::Commerce.price,
+
+#     stock_quantity: Faker::Number.between(from: 1, to: 100)
+#   )
+# end
+products.each do |product|
+  category = Category.find_or_create_by(name: product["category"])
+  if category && category.valid?
+    new_product = category.product.create(
+      name: product["name"],
+      price: product["price"],
+      description: product["description"],
+      stock_quantity: product["stock quantity"]
+    )
+    unless new_product && new_product.valid?
+      puts "Failed to create the movie #{product["name"]}"
+            next
+    end
+  else
+    puts "Something is wrong creating this category:
+        #{product["category"]} for the product #{product["name"]}"
+  end
 end
 
-puts "There are now #{Product.count} Products"
+puts "Create #{Category.count} categories."
+puts "Create #{Product.count} Products"
